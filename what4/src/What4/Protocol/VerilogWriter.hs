1f953d4656b73a5cdec6f84cf004698be82fdd97
Aaron Tomb <atomb@galois.com>
2021-05-14 11:01:04 -0700

    Support client-provided Verilog input lists
    Makes progress in moving away from String along the way

--- what4/src/What4/Protocol/VerilogWriter.hs
+++ what4/src/What4/Protocol/VerilogWriter.hs
@@ -16,10 +16,12 @@ module What4.Protocol.VerilogWriter
   ) where
 
 import Control.Monad.Except
+import Data.Parameterized.Nonce (Nonce(..))
 import Data.Parameterized.Some (Some(..), traverseSome)
+import Data.Text (Text)
 import Prettyprinter
 import What4.Expr.Builder (Expr, SymExpr)
-import What4.Interface (IsExprBuilder)
+import What4.Interface (IsExprBuilder, BaseTypeRepr)
 
 import What4.Protocol.VerilogWriter.AST
 import What4.Protocol.VerilogWriter.ABCVerilog
@@ -31,16 +33,18 @@ import What4.Protocol.VerilogWriter.Backend
 exprsVerilog ::
   (IsExprBuilder sym, SymExpr sym ~ Expr n) =>
   sym ->
+  [(Some (Expr n), Text)] ->
   [Some (Expr n)] ->
   Doc () ->
   ExceptT String IO (Doc ())
-exprsVerilog sym es name = fmap (\m -> moduleDoc m name) (exprsToModule sym es)
+exprsVerilog sym ins es name = fmap (\m -> moduleDoc m name) (exprsToModule sym ins es)
 
 -- | Convert the given What4 expressions, representing the outputs of a
 -- circuit, into a Verilog module of the given name.
 exprsToModule ::
   (IsExprBuilder sym, SymExpr sym ~ Expr n) =>
   sym ->
+  [(Some (Expr n), Text)] ->
   [Some (Expr n)] ->
   ExceptT String IO (Module sym n)
-exprsToModule sym es = mkModule sym $ map (traverseSome exprToVerilogExpr) es
+exprsToModule sym ins es = mkModule sym ins $ map (traverseSome exprToVerilogExpr) es
