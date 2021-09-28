2a45ef67510330c10f3b089218a95d8d5d231ef2
Rob Dockins <rdockins@galois.com>
2021-09-27 17:10:14 -0700

    More documentation.  Add range and domain information about special functions.  A few additional typeclass instances.

--- what4/src/What4/SpecialFunctions.hs
+++ what4/src/What4/SpecialFunctions.hs
@@ -31,15 +31,26 @@ Some popular constant values are also included.
 -}
 
 module What4.SpecialFunctions
-  ( R
+  ( -- * Representation of special functions
+    R
   , SpecialFunction(..)
-  , buildSpecialFnArgs
+
+    -- ** Symmetry properties of special functions
   , FunctionSymmetry(..)
   , specialFnSymmetry
+
+    -- ** Packaging arguments to special functions
   , SpecialFnArg(..)
   , traverseSpecialFnArg
   , SpecialFnArgs(..)
   , traverseSpecialFnArgs
+
+    -- ** Interval data for domain and range
+  , RealPoint(..)
+  , RealBound(..)
+  , RealInterval(..)
+  , specialFnDomain
+  , specialFnRange
   ) where
 
 import           Data.Kind (Type)
@@ -50,15 +61,36 @@ import           Data.Parameterized.Ctx
 import           Data.Parameterized.TH.GADT
 import           Data.Parameterized.TraversableFC
 
+-- | Some special functions exhibit useful symmetries in their arguments.
+--   A function @f@ is an odd function if @f(-x) = -f(x)@, and is even
+--   if @f(-x) = f(x)@.  We extend this notion to arguments of more than
+--   one function by saying that a function is even/odd in its @i@th
+--   argument if it is even/odd when the other arguments are fixed.
 data FunctionSymmetry r
   = NoSymmetry
   | EvenFunction
   | OddFunction
  deriving (Show)
 
+
+-- | Phantom data index representing the real number line.
+--   Used for specifying the arity of special functions.
 data R
 
--- | Data type for representing
+-- | Data type for representing \"special\" functions.
+--   These include functions from standard and hyperbolic
+--   trigonometry, exponential and logarithmic functions,
+--   as well as other well-known mathematical functions.
+--
+--   Generally, little solver support exists for such functions
+--   (although systems like dReal and Metatarski can prove some
+--   properties).  Nonetheless, we may have some information about
+--   specific values these functions take, the domains on which they
+--   are defined, or the range of values their outputs may take, or
+--   specific relationships that may exists between these functions
+--   (e.g., trig identities).  This information may, in some
+--   circumstances, be sufficent to prove properties of interest, even
+--   if the functions cannot be represented in their entirety.
 data SpecialFunction (args :: Ctx Type) where
   -- constant values involving Pi
   Pi             :: SpecialFunction EmptyCtx -- pi
@@ -156,6 +188,52 @@ instance Show (SpecialFunction args) where
     Exp10          -> "exp10"
     Log10          -> "log10"
 
+-- | Values that can appear in the definition of domain and
+--   range intervals for special functions.
+data RealPoint
+  = Zero
+  | NegOne
+  | PosOne
+  | NegInf
+  | PosInf
+  | NegPi
+  | PosPi
+  | NegHalfPi
+  | PosHalfPi
+
+instance Show RealPoint where
+  show Zero   = "0"
+  show NegOne = "-1"
+  show PosOne = "+1"
+  show NegInf = "-∞"
+  show PosInf = "+∞"
+  show NegPi  = "-π"
+  show PosPi  = "+π"
+  show NegHalfPi = "-π/2"
+  show PosHalfPi = "+π/2"
+
+-- | The endpoint of an interval, which may be inclusive or exclusive.
+data RealBound
+  = Incl RealPoint
+  | Excl RealPoint
+
+-- | An interval on real values, or a point.
+data RealInterval r where
+  RealPoint    :: SpecialFunction EmptyCtx -> RealInterval R
+  RealInterval :: RealBound -> RealBound -> RealInterval R
+
+instance Show (RealInterval r) where
+  show (RealPoint x) = show x
+  show (RealInterval lo hi) = lostr ++ ", " ++ histr
+    where
+      lostr = case lo of
+                Incl x -> "[" ++ show x
+                Excl x -> "(" ++ show x
+      histr = case hi of
+                Incl x -> show x ++ "]"
+                Excl x -> show x ++ ")"
+
+-- | Compute function symmetry information for the given special function.
 specialFnSymmetry :: SpecialFunction args -> Ctx.Assignment FunctionSymmetry args
 specialFnSymmetry fn = case fn of
     Pi             -> Ctx.Empty
@@ -183,7 +261,7 @@ specialFnSymmetry fn = case fn of
     Cosh           -> Ctx.Empty :> EvenFunction
     Tanh           -> Ctx.Empty :> OddFunction
     Arcsinh        -> Ctx.Empty :> OddFunction
-    Arccosh        -> Ctx.Empty :> NoSymmetry -- TODO? is it actually even?
+    Arccosh        -> Ctx.Empty :> NoSymmetry
     Arctanh        -> Ctx.Empty :> OddFunction
 
     Exp            -> Ctx.Empty :> NoSymmetry
@@ -195,12 +273,64 @@ specialFnSymmetry fn = case fn of
     Exp10          -> Ctx.Empty :> NoSymmetry
     Log10          -> Ctx.Empty :> NoSymmetry
 
-    Hypot          -> Ctx.Empty :> NoSymmetry :> NoSymmetry
-    Arctan2        -> Ctx.Empty :> NoSymmetry :> NoSymmetry -- TODO? is it actually odd in both arguments?
+    Hypot          -> Ctx.Empty :> EvenFunction :> EvenFunction
+    Arctan2        -> Ctx.Empty :> OddFunction :> NoSymmetry
 
 
-buildSpecialFnArgs :: f R -> SpecialFunction args -> Ctx.Assignment f args
-buildSpecialFnArgs repr fn = case fn of
+-- | Compute the range of values that may be returned by the given special function
+--   as its arguments take on the possible values of its domain.  This may include
+--   limiting values if the function's domain includes infinities; for example
+--   @exp(-inf) = 0@.
+specialFnRange :: SpecialFunction args -> RealInterval R
+specialFnRange fn = case fn of
+    Pi             -> RealPoint Pi
+    HalfPi         -> RealPoint HalfPi
+    QuarterPi      -> RealPoint QuarterPi
+    OneOverPi      -> RealPoint OneOverPi
+    TwoOverPi      -> RealPoint TwoOverPi
+    TwoOverSqrt_Pi -> RealPoint TwoOverSqrt_Pi
+    Sqrt_2         -> RealPoint Sqrt_2
+    Sqrt_OneHalf   -> RealPoint Sqrt_OneHalf
+    E              -> RealPoint E
+    Log2_E         -> RealPoint Log2_E
+    Log10_E        -> RealPoint Log10_E
+    Ln_2           -> RealPoint Ln_2
+    Ln_10          -> RealPoint Ln_10
+
+    Sin            -> RealInterval (Incl NegOne) (Incl PosOne)
+    Cos            -> RealInterval (Incl NegOne) (Incl PosOne)
+    Tan            -> RealInterval (Incl NegInf) (Incl PosInf)
+
+    Arcsin         -> RealInterval (Incl NegHalfPi) (Incl PosHalfPi)
+    Arccos         -> RealInterval (Incl Zero)      (Incl PosPi)
+    Arctan         -> RealInterval (Incl NegHalfPi) (Incl PosHalfPi)
+
+    Sinh           -> RealInterval (Incl NegInf) (Incl PosInf)
+    Cosh           -> RealInterval (Incl PosOne) (Incl PosInf)
+    Tanh           -> RealInterval (Incl NegOne) (Incl PosOne)
+    Arcsinh        -> RealInterval (Incl NegInf) (Incl PosInf)
+    Arccosh        -> RealInterval (Incl Zero)   (Incl PosInf)
+    Arctanh        -> RealInterval (Incl NegInf) (Incl PosInf)
+
+    Exp            -> RealInterval (Incl Zero)   (Incl PosInf)
+    Log            -> RealInterval (Incl NegInf) (Incl PosInf)
+    Expm1          -> RealInterval (Incl NegOne) (Incl PosInf)
+    Log1p          -> RealInterval (Incl NegInf) (Incl PosInf)
+    Exp2           -> RealInterval (Incl Zero)   (Incl PosInf)
+    Log2           -> RealInterval (Incl NegInf) (Incl PosInf)
+    Exp10          -> RealInterval (Incl Zero)   (Incl PosInf)
+    Log10          -> RealInterval (Incl NegInf) (Incl PosInf)
+
+    Hypot          -> RealInterval (Incl Zero) (Incl PosInf)
+    Arctan2        -> RealInterval (Incl NegPi) (Incl PosPi)
+
+
+-- | Compute the domain of the given special function.  As a mathematical
+--   entity, the value of the given function is not well-defined outside
+--   its domain. In floating-point terms, a special function will return
+--   a @NaN@ when evaluated on arguments outside its domain.
+specialFnDomain :: SpecialFunction args -> Ctx.Assignment RealInterval args
+specialFnDomain fn = case fn of
     Pi             -> Ctx.Empty
     HalfPi         -> Ctx.Empty
     QuarterPi      -> Ctx.Empty
@@ -215,36 +345,39 @@ buildSpecialFnArgs repr fn = case fn of
     Ln_2           -> Ctx.Empty
     Ln_10          -> Ctx.Empty
 
-    Sin            -> Ctx.Empty :> repr
-    Cos            -> Ctx.Empty :> repr
-    Tan            -> Ctx.Empty :> repr
-    Arcsin         -> Ctx.Empty :> repr
-    Arccos         -> Ctx.Empty :> repr
-    Arctan         -> Ctx.Empty :> repr
+    Sin            -> Ctx.Empty :> RealInterval (Excl NegInf) (Excl PosInf)
+    Cos            -> Ctx.Empty :> RealInterval (Excl NegInf) (Excl PosInf)
+    Tan            -> Ctx.Empty :> RealInterval (Excl NegInf) (Excl PosInf)
+    Arcsin         -> Ctx.Empty :> RealInterval (Incl NegOne) (Incl PosOne)
+    Arccos         -> Ctx.Empty :> RealInterval (Incl NegOne) (Incl PosOne)
+    Arctan         -> Ctx.Empty :> RealInterval (Incl NegInf) (Incl PosInf)
 
-    Sinh           -> Ctx.Empty :> repr
-    Cosh           -> Ctx.Empty :> repr
-    Tanh           -> Ctx.Empty :> repr
-    Arcsinh        -> Ctx.Empty :> repr
-    Arccosh        -> Ctx.Empty :> repr
-    Arctanh        -> Ctx.Empty :> repr
+    Sinh           -> Ctx.Empty :> RealInterval (Incl NegInf) (Incl PosInf)
+    Cosh           -> Ctx.Empty :> RealInterval (Incl NegInf) (Incl PosInf)
+    Tanh           -> Ctx.Empty :> RealInterval (Incl NegInf) (Incl PosInf)
+    Arcsinh        -> Ctx.Empty :> RealInterval (Incl NegInf) (Incl PosInf)
+    Arccosh        -> Ctx.Empty :> RealInterval (Incl PosOne) (Incl PosInf)
+    Arctanh        -> Ctx.Empty :> RealInterval (Incl NegOne) (Incl PosOne)
 
-    Exp            -> Ctx.Empty :> repr
-    Log            -> Ctx.Empty :> repr
-    Expm1          -> Ctx.Empty :> repr
-    Log1p          -> Ctx.Empty :> repr
-    Exp2           -> Ctx.Empty :> repr
-    Log2           -> Ctx.Empty :> repr
-    Exp10          -> Ctx.Empty :> repr
-    Log10          -> Ctx.Empty :> repr
+    Exp            -> Ctx.Empty :> RealInterval (Incl NegInf) (Incl PosInf)
+    Log            -> Ctx.Empty :> RealInterval (Incl Zero)   (Incl PosInf)
+    Expm1          -> Ctx.Empty :> RealInterval (Incl NegInf) (Incl PosInf)
+    Log1p          -> Ctx.Empty :> RealInterval (Incl NegOne) (Incl PosInf)
+    Exp2           -> Ctx.Empty :> RealInterval (Incl NegInf) (Incl PosInf)
+    Log2           -> Ctx.Empty :> RealInterval (Incl Zero)   (Incl PosInf)
+    Exp10          -> Ctx.Empty :> RealInterval (Incl NegInf) (Incl PosInf)
+    Log10          -> Ctx.Empty :> RealInterval (Incl Zero)   (Incl PosInf)
 
-    Hypot          -> Ctx.Empty :> repr :> repr
-    Arctan2        -> Ctx.Empty :> repr :> repr
+    Hypot          -> Ctx.Empty :> RealInterval (Incl NegInf) (Incl PosInf)
+                                :> RealInterval (Incl NegInf) (Incl PosInf)
+    Arctan2        -> Ctx.Empty :> RealInterval (Incl NegInf) (Incl PosInf)
+                                :> RealInterval (Incl NegInf) (Incl PosInf)
 
-
+-- | Data type for wrapping the actual arguments to special functions.
 data SpecialFnArg (e :: k -> Type) (tp::k) (r::Type) where
   SpecialFnArg :: e tp -> SpecialFnArg e tp R
 
+-- | Data type for wrapping a collction of actual arguments to special functions.
 newtype SpecialFnArgs (e :: k -> Type) (tp :: k) args =
   SpecialFnArgs (Ctx.Assignment (SpecialFnArg e tp) args)
 
@@ -259,24 +392,40 @@ instance Hashable (SpecialFunction args) where
 instance TestEquality SpecialFunction where
   testEquality = $(structuralTypeEquality [t|SpecialFunction|] [])
 
+instance OrdF SpecialFunction where
+  compareF = $(structuralTypeOrd [t|SpecialFunction|] [])
+
+
 instance OrdF e => TestEquality (SpecialFnArg e tp) where
   testEquality (SpecialFnArg x) (SpecialFnArg y) =
     do Refl <- testEquality x y
        return Refl
 
+instance OrdF e => OrdF (SpecialFnArg e tp) where
+  compareF (SpecialFnArg x) (SpecialFnArg y) =
+    case compareF x y of
+      LTF -> LTF
+      EQF -> EQF
+      GTF -> GTF
+
 instance HashableF e => HashableF (SpecialFnArg e tp) where
   hashWithSaltF s (SpecialFnArg x) = hashWithSaltF s x
+
+
+instance OrdF e => Eq (SpecialFnArgs e tp r) where
+  SpecialFnArgs xs == SpecialFnArgs ys = xs == ys
+
+instance OrdF e => Ord (SpecialFnArgs e tp r) where
+  compare (SpecialFnArgs xs) (SpecialFnArgs ys) = compare xs ys
+
+instance HashableF e => Hashable (SpecialFnArgs e tp args) where
+  hashWithSalt s (SpecialFnArgs xs) = hashWithSaltF s xs
+
 
 traverseSpecialFnArg :: Applicative m =>
   (e tp -> m (f tp)) ->
   SpecialFnArg e tp r -> m (SpecialFnArg f tp r)
 traverseSpecialFnArg f (SpecialFnArg x) = SpecialFnArg <$> f x
-
-instance OrdF e => Eq (SpecialFnArgs e tp r) where
-  SpecialFnArgs xs == SpecialFnArgs ys = xs == ys
-
-instance HashableF e => Hashable (SpecialFnArgs e tp args) where
-  hashWithSalt s (SpecialFnArgs xs) = hashWithSaltF s xs
 
 traverseSpecialFnArgs :: Applicative m =>
   (e tp -> m (f tp)) ->
