ef18c3fac46976f24ac467b2d372ab8f8f472abe
arjunvish <arjun.viswanathan23@gmail.com>
2022-08-30 14:14:30 -0500

    Stable abduction tests

--- what4/test/Abduct.hs
+++ what4/test/Abduct.hs
@@ -8,6 +8,7 @@ import           Test.Tasty
 import           Test.Tasty.HUnit
 
 import Data.Foldable (forM_)
+import Data.List (length)
 import System.IO (FilePath, IOMode(..), openFile, hClose)
 import System.IO.Temp (withSystemTempFile)
 import Data.Text
@@ -34,27 +35,29 @@ import What4.Protocol.Online
 cvc5executable :: FilePath
 cvc5executable = "cvc5"
 
+-- Call the online getAbduct tactic
 testGetAbductOnline ::
   ExprBuilder t st fs ->
   [BoolExpr t] ->
   BoolExpr t ->
-  IO ()
-testGetAbductOnline sym hs g = do
+  Int ->
+  IO [String]
+testGetAbductOnline sym hs g n = do
+  -- Print SMT file in /tmp/
   withSystemTempFile "what4abdonline" $ \fname mirroredOutput -> do
     proc <- startSolverProcess @(SMT2.Writer CVC5) cvc5Features (Just mirroredOutput) sym
     let conn = solverConn proc
     inNewFrame proc $ do
       mapM_ (\x -> assume conn x) hs
-      res <- getAbducts proc 5 (pack "abd") g
-      putStrLn ("Abducts:")
-      forM_ res putStrLn
-    hClose mirroredOutput
+      getAbducts proc n (pack "abd") g
+    -- hClose mirroredOutput
 
+-- Call the offline getAbduct tactic
 testGetAbductOffline ::
   ExprBuilder t st fs ->
   BoolExpr t ->
   Int ->
-  IO ()
+  IO [String]
 testGetAbductOffline sym f n = do
   -- Print SMT file in /tmp/
   withSystemTempFile "what4abdoffline" $ \fname mirroredOutput -> do
@@ -64,19 +67,17 @@ testGetAbductOffline sym f n = do
                            , logHandle = Just mirroredOutput }
     withCVC5 sym cvc5executable logData $ \session -> do
       f_term <- mkSMTTerm (sessionWriter session) f
-      putStrLn "Abducts:"
-      abd <- runGetAbducts session n (pack "abd") f_term 
-      forM_ abd putStrLn
-    hClose mirroredOutput
+      runGetAbducts session n (pack "abd") f_term
+    --hClose mirroredOutput
 
--- | Determine whether a predicate is satisfiable, and print out the values of a
--- set of expressions if a satisfying instance is found.
+-- Prove f using an SMT solver, by checking if ~f is unsatisfiable
 prove ::
   ExprBuilder t st fs ->
   BoolExpr t ->
   [(String, IntegerExpr t)] ->
   IO (SatResult () ())
 prove sym f es = do
+  -- Print SMT file in /tmp/
   withSystemTempFile "what4prove" $ \fname mirroredOutput -> do
     proc <- startSolverProcess @(SMT2.Writer CVC5) cvc5Features (Just mirroredOutput) sym
     let logData = LogData { logCallbackVerbose = \_ _ -> return ()
@@ -89,16 +90,45 @@ prove sym f es = do
     withCVC5 sym cvc5executable logData $ \session -> do
       checkSatisfiable proc "test" notf
 
-testSatAbd :: TestTree
-testSatAbd = testCase "testing SAT query for abduction" $ do
+-- Tests
+
+testAbdOnline :: ExprBuilder t st fs -> 
+  [BoolExpr t] -> 
+  BoolExpr t -> 
+  TestTree
+testAbdOnline sym hs g = testCase "getting 3 abducts using cvc5 online" $ do
+  -- Ask for 3 abducts for f
+  res <- testGetAbductOnline sym hs g 3
+  (Data.List.length res == 3) @? "3 online abducts"
+
+testAbdOffline :: ExprBuilder t st fs -> 
+  BoolExpr t -> 
+  [(String, IntegerExpr t)] -> 
+  TestTree
+testAbdOffline sym f es = testCase "getting 3 abducts using cvc5 offline" $ do
+  -- Ask for 3 abducts for f
+  res <- testGetAbductOffline sym f 3
+  (Data.List.length res == 3) @? "3 offline abducts"
+
+testSatAbd :: ExprBuilder t st fs -> 
+  BoolExpr t -> 
+  [(String, IntegerExpr t)] -> 
+  TestTree
+testSatAbd sym f es = testCase "testing SAT query for abduction" $ do
+  -- Prove f (is ~f unsatisfiable?). We expect ~f to be satisfiable
+  res <- prove sym f es
+  isSat res @? "sat"
+
+
+main :: IO ()
+main = do
   Some ng <- newIONonceGenerator
   sym <- newExprBuilder FloatIEEERepr EmptyExprBuilderState ng
 
   -- This line is necessary for working with cvc5.
   extendConfig cvc5Options (getConfiguration sym)
 
-  -- The following is expected to be satisfiable. Let's check with cvc5
-  --   not (y >= 0 => (x + y + z) >= 0)
+  -- Build this formula: ~(y >= 0 => (x + y + z) >= 0)
   
   -- First, declare fresh constants for each of the three variables x, y, z.
   x <- freshConstant sym (safeSymbol "x") BaseIntegerRepr
@@ -112,52 +142,17 @@ testSatAbd = testCase "testing SAT query for abduction" $ do
   xyzgte0 <- intLe sym zero pxyz          -- 0 <= (x + y + z) 
   f <- impliesPred sym ygte0 xyzgte0      -- (0 <= y) -> (0 <= (x + y + z))
 
-  -- Prove f (is ~f unsatisfiable?), and otherwise, print countermodel and a formula that will allow me to prove f
-  putStrLn "Offline SMT calls"
-  res <- prove sym f [ ("x", x)
-                     , ("y", y)
-                     , ("z", z)
-                     ]
-  isUnsat res @? "unsat"
-
-main :: IO ()
-main = do
-  {-Some ng <- newIONonceGenerator
-  sym <- newExprBuilder FloatIEEERepr EmptyExprBuilderState ng
-
-  -- This line is necessary for working with cvc5.
-  extendConfig cvc5Options (getConfiguration sym)
-
-  -- The following is expected to be satisfiable. Let's check with cvc5
-  --   not (y >= 0 => (x + y + z) >= 0)
-  
-  -- First, declare fresh constants for each of the three variables x, y, z.
-  x <- freshConstant sym (safeSymbol "x") BaseIntegerRepr
-  y <- freshConstant sym (safeSymbol "y") BaseIntegerRepr
-  z <- freshConstant sym (safeSymbol "z") BaseIntegerRepr
-
-  -- Next, build up the clause
-  zero <- intLit sym 0                        -- 0
-  pxyz <- intAdd sym x =<< intAdd sym y z -- x + y + z
-  ygte0 <- intLe sym zero y               -- 0 <= y
-  xyzgte0 <- intLe sym zero pxyz          -- 0 <= (x + y + z) 
-  f <- impliesPred sym ygte0 xyzgte0      -- (0 <= y) -> (0 <= (x + y + z))
-
-  -- Prove f (is ~f unsatisfiable?), and otherwise, print countermodel and a formula that will allow me to prove f
-  putStrLn "Offline SMT calls"
-  res <- prove sym f [ ("x", x)
-                     , ("y", y)
-                     , ("z", z)
-                     ]
-  if (isSat res) then do
-    -- This query to the SMT solver: (get-abduct A (=> (>= y 0) (>= (+ x y z) 0)))
-    putStrLn "\nOnline call to get-abduct"
-    testGetAbductOnline sym [ygte0] xyzgte0
-    putStrLn "\n\nOffline call to get-abduct"
-    testGetAbductOffline sym f 5
-  else if (isUnsat res) then
-    putStrLn "\ncvc5 proved goal"
-  else
-    putStrLn "\ncvc5 returned Unknown!"-}
   defaultMain $ testGroup "Tests" $
-    [ testSatAbd ]
+    [ -- test passes if f is disproved (~f is sat)
+      testSatAbd sym f [ ("x", x)
+                     , ("y", y)
+                     , ("z", z)
+                           ],
+      -- test passes if cvc5 returns 3 abducts (offline)
+      testAbdOffline sym f [ ("x", x)
+                     , ("y", y)
+                     , ("z", z)
+                     ],
+      -- test passes if cvc5 returns 3 abducts (online)
+      testAbdOnline sym [ygte0] xyzgte0
+    ]
