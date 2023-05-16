f471faa0cb597cc1987e3b0f53312ff4cc49d653
Andrei <andrei@stefanescu.io>
2023-05-16 23:26:35 +0000

    Parse SMTLib extract and associative operators.

--- what4/src/What4/Protocol/SMTLib2.hs
+++ what4/src/What4/Protocol/SMTLib2.hs
@@ -106,6 +106,7 @@ import           Control.Monad.Reader
 import qualified Data.Bimap as Bimap
 import qualified Data.BitVector.Sized as BV
 import           Data.Char (digitToInt, isAscii)
+import           Data.Foldable
 import           Data.HashMap.Lazy (HashMap)
 import qualified Data.HashMap.Lazy as HashMap
 import           Data.IORef
@@ -1063,6 +1064,15 @@ data Op sym where
       Op sym
 
 data Assoc = RightAssoc | LeftAssoc
+  deriving (Eq, Ord, Show)
+
+opAssoc :: Op sym -> Maybe Assoc
+opAssoc = \case
+  Op1{} -> Nothing
+  Op2 _ assoc _ -> assoc
+  BVOp1{} -> Nothing
+  BVOp2 assoc _ -> assoc
+  BVComp2{} -> Nothing
 
 newtype Processor sym a = Processor (ExceptT String (ReaderT (ProcessorEnv sym) IO) a)
   deriving (Functor, Applicative, Monad, MonadIO, MonadError String, MonadReader (ProcessorEnv sym))
@@ -1166,6 +1176,24 @@ parseExpr sym sexp = case sexp of
     BVProof{} <- getBVProof arg1_expr
     BVProof{} <- getBVProof arg2_expr
     liftIO $ Some <$> I.bvConcat sym arg1_expr arg2_expr
+  SApp [SApp ["_", "extract", i_sexp, j_sexp], arg]
+    | Just i <- parseIntSolverValue i_sexp
+    , Just j <- parseIntSolverValue j_sexp -> do
+      let n = i - j + 1
+      Some j_repr <- return $ mkNatRepr $ fromIntegral j
+      Some n_repr <- return $ mkNatRepr $ fromIntegral n
+      Some arg_expr <- parseExpr sym arg
+      BVProof w_repr <- getBVProof arg_expr
+      case (isPosNat n_repr, testLeq (addNat j_repr n_repr) w_repr) of
+        (Just LeqProof, Just LeqProof) ->
+          liftIO $ Some <$> I.bvSelect sym j_repr n_repr arg_expr
+        _ -> throwError ""
+  SApp ((SAtom operator) : operands)
+    | Just op <- HashMap.lookup operator (opTable @sym)
+    , Just assoc <- opAssoc op
+    , length operands > 2 -> case assoc of
+      LeftAssoc -> parseExpr sym $ foldl' (\acc arg -> SApp [SAtom operator, acc, arg]) (head operands) (tail operands)
+      RightAssoc -> parseExpr sym $ foldr' (\arg acc -> SApp [SAtom operator, arg, acc]) (last operands) (init operands)
   SApp ((SAtom operator) : operands) -> case HashMap.lookup operator (opTable @sym) of
     Just (Op1 arg_types fn) -> do
       args <- mapM (parseExpr sym) operands
