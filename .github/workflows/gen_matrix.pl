3a425a85d5e521b41eb491e2163e2456b5f63158
Ryan Scott <rscott@galois.com>
2023-06-30 15:38:06 -0400

    gen_matrix.pl: Set main GHC version to 9.2

--- .github/workflows/gen_matrix.pl
+++ .github/workflows/gen_matrix.pl
@@ -72,7 +72,7 @@ solver(abc).
 %% version (which is not necessarily the "most recently released"
 %% version.
 main_version(ubuntu, "ubuntu-latest").
-main_version(ghc, "8.10.7").
+main_version(ghc, "9.2.2").
 main_version(z3, "4_8_14").
 main_version(yices, "2_6_4").
 main_version(stp, "2_3_3").
